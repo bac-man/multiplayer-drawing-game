3007cdf02d9983d9ab1c2cb81e31e986d2f93778
bac-man <90024198+bac-man@users.noreply.github.com>
2023-05-09 13:41:44 +0300

    Add webpack file extension resolve options

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,9 @@ module.exports = {
     port: "3000",
     open: true,
   },
+  resolve: {
+    extensions: [".jsx", ".js"],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./public/index.html",
