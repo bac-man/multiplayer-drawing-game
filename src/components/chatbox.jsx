05ea02231fbb8403aa4f0e86e5237eb6b896ad13
bac-man <90024198+bac-man@users.noreply.github.com>
2023-05-11 10:17:06 +0300

    Add basic layout with placeholder chatbox

---
+++ src/components/chatbox.jsx
@@ -0,0 +1,7 @@
+import style from "./chatbox.module.scss";
+
+const Chatbox = ({}) => {
+  return <div className={style.chatbox} />;
+};
+
+export default Chatbox;
--- src/components/gameContainer.jsx
+++ src/components/gameContainer.jsx
@@ -1,10 +1,14 @@
+import Chatbox from "./chatbox";
 import GameCanvas from "./gameCanvas";
 import style from "./gameContainer.module.scss";
 
 const GameContainer = ({}) => {
   return (
     <div className={style.container}>
-      <GameCanvas />
+      <div className={style.wrapper}>
+        <GameCanvas />
+        <Chatbox />
+      </div>
     </div>
   );
 };
