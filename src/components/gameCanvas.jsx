ea386a683110082d4d30caccda7cb67f9a7b145a
bac-man <90024198+bac-man@users.noreply.github.com>
2023-05-12 11:04:35 +0300

    Add basic canvas mouse drawing logic

--- src/components/gameCanvas.jsx
+++ src/components/gameCanvas.jsx
@@ -1,7 +1,77 @@
+import { useEffect, useRef, useState } from "react";
 import style from "./gameCanvas.module.scss";
 
 const GameCanvas = ({}) => {
-  return <canvas className={style.canvas} />;
+  const canvasRef = useRef();
+  const [mouseDown, setMouseDown] = useState(false);
+  const [currentLinePoints, setCurrentLinePoints] = useState([]);
+  const drawingOptions = useRef();
+
+  useEffect(() => {
+    drawingOptions.current = {
+      ctx: canvasRef.current.getContext("2d"),
+      lineCap: "round",
+      lineWidth: 6,
+      strokeStyle: "rgb(0, 0, 0);",
+    };
+  }, []);
+
+  const drawLinePoint = (e) => {
+    const options = drawingOptions.current;
+    const ctx = options.ctx;
+    ctx.lineCap = options.lineCap;
+    ctx.lineWidth = options.lineWidth;
+    ctx.strokeStyle = options.strokeStyle;
+    const canvas = canvasRef.current;
+    const canvasRect = canvas.getBoundingClientRect();
+
+    const xScale =
+      window.getComputedStyle(canvas).getPropertyValue("width").split("px")[0] /
+      canvas.width;
+    const yScale =
+      window
+        .getComputedStyle(canvas)
+        .getPropertyValue("height")
+        .split("px")[0] / canvas.height;
+
+    const posX = (e.clientX - canvasRect.left) / xScale;
+    const posY = (e.clientY - canvasRect.top) / yScale;
+
+    ctx.beginPath();
+    ctx.moveTo(posX, posY);
+    ctx.lineTo(posX, posY);
+    ctx.stroke();
+
+    const previousPoint = currentLinePoints[currentLinePoints.length - 1];
+    if (previousPoint) {
+      ctx.lineTo(previousPoint.x, previousPoint.y);
+      ctx.stroke();
+    }
+
+    currentLinePoints.push({ x: posX, y: posY });
+  };
+
+  const startDrawingLine = (e) => {
+    drawLinePoint(e);
+    setMouseDown(true);
+  };
+
+  return (
+    <canvas
+      ref={canvasRef}
+      className={style.canvas}
+      onMouseDown={startDrawingLine}
+      onMouseUp={() => {
+        setMouseDown(false);
+        setCurrentLinePoints([]);
+      }}
+      onMouseMove={(e) => {
+        if (mouseDown) {
+          drawLinePoint(e);
+        }
+      }}
+    />
+  );
 };
 
 export default GameCanvas;
