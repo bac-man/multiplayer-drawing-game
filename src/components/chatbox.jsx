e5af51aea23ce3f5af26f77dd3d5639e257ac1af
bac-man <90024198+bac-man@users.noreply.github.com>
2023-05-14 09:53:36 +0300

    Add chatbox elements, adjust canvas/chatbox ratio

--- src/components/chatbox.jsx
+++ src/components/chatbox.jsx
@@ -1,7 +1,15 @@
 import style from "./chatbox.module.scss";
 
 const Chatbox = ({}) => {
-  return <div className={style.chatbox} />;
+  return (
+    <div className={style.chatbox}>
+      <div className={style.messageHistory} />
+      <div className={style.inputWrapper}>
+        <input type={"text"} />
+        <button>Send</button>
+      </div>
+    </div>
+  );
 };
 
 export default Chatbox;
